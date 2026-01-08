3f33000ca74d179d3b0663162a1999995d11017b
Beastly713 <beastlymadness420@gmail.com>
2026-01-09 01:54:07 +0530

    feat: add CLI entry point and root command

---
+++ cmd/root.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "horcrux",
+	Short: "Split your file into encrypted fragments",
+	Long: `Horcrux: A secure tool to split your files into encrypted fragments 
+(horcruxes) requiring a specific threshold to reconstruct.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Beastly713/horcrux/cmd" 
+)
+
+func main() {
+	cmd.Execute()
+}
